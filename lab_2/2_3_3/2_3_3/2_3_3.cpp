42b983cddbc9e69b9900b716c2ba9d6598217e37
MiNeBudemRabami <sho_mLyaaa@mail.ru>
2019-03-20 00:46:02 +0300

    ...

---
+++ lab_2/2_3_3/2_3_3/2_3_3.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+void print(string & word)
+{
+	cout << word;
+}
+
+void toLow(string & inputWord)
+{
+
+	for (unsigned int i = 0; i < inputWord.size(); i++)
+	{
+		
+	}
+}
+
+void savingChanges()
+{
+}
+
+void translate(string & inputWord)
+{
+	toLow(inputWord);
+
+}
+
+int main()
+{
+	string inputWord = "";
+	for (;;)
+	{
+		cin >> inputWord;
+
+		if (inputWord == "...")
+		{
+			savingChanges();
+			break;
+		}
+
+		translate(inputWord);
+
+	}
+}
