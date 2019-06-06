33e811ab0ac84663984e18f396749746e9604a72
MiNeBudemRabami <sho_mLyaaa@mail.ru>
2019-06-06 17:18:17 +0300

    ...

--- lab_4/4_1_2/4_1_2/4_1_2.cpp
+++ lab_4/4_1_2/4_1_2/4_1_2.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <functional>
 #include "IShape.h"
 #include "triangle.h"
 #include "rectangle.h"
@@ -11,6 +13,8 @@
 #include <deque>
 
 using namespace std;
+
+using Handler = std::function<void(vector <string> const& parametrSet, vector <unique_ptr<IShape>> &shapeSet)>;
 
 static bool compareArea(unique_ptr<IShape> const& shape1, unique_ptr<IShape> const& shape2)
 {
@@ -69,27 +73,15 @@ int main()
 	cout << std::fixed;
 
 	vector <unique_ptr<IShape>> shapeSet;
-
-	for (;;)
-	{
-		vector <string> parametrSet = readCommand();
-
-		if (cin.eof())
-		{
-			break;
-		}
-
-		else if (parametrSet[0] == "circle")
-		{
+	map<string, Handler> actionMap = {
+		{ "circle", [](vector <string> const& parametrSet, vector <unique_ptr<IShape>> &shapeSet) {
 			Point center;
 			center.x = stod(parametrSet[1]);
 			center.y = stod(parametrSet[2]);
 
 			shapeSet.push_back(make_unique<Circle>(center, stod(parametrSet[3]), stol(parametrSet[4], 0, 16), stol(parametrSet[5], 0, 16)));
-		}
-
-		else if (parametrSet[0] == "LineSegment")
-		{
+		} },
+		{ "LineSegment", [](vector <string> const& parametrSet, vector <unique_ptr<IShape>> &shapeSet) {
 			Point startPoint;
 			startPoint.x = stod(parametrSet[1]);
 			startPoint.y = stod(parametrSet[2]);
@@ -99,19 +91,15 @@ int main()
 			endPoint.y = stod(parametrSet[2]);
 
 			shapeSet.push_back(make_unique<LineSegment>(startPoint, endPoint, stol(parametrSet[4], 0, 16)));
-		}
-
-		else if (parametrSet[0] == "rectangle")
-		{
+		} },
+		{ "rectangle", [](vector <string> const& parametrSet, vector <unique_ptr<IShape>> &shapeSet) {
 			Point topLeft;
 			topLeft.x = stod(parametrSet[1]);
 			topLeft.y = stod(parametrSet[2]);
 
 			shapeSet.push_back(make_unique<RectangleC>(topLeft, stod(parametrSet[3]), stod(parametrSet[4]), stol(parametrSet[5], 0, 16), stol(parametrSet[6], 0, 16)));
-		}
-
-		else if (parametrSet[0] == "triangle")
-		{
+		} },
+		{ "triangle", [](vector <string> const& parametrSet, vector <unique_ptr<IShape>> &shapeSet) {
 			Point vertex1;
 			vertex1.x = stod(parametrSet[1]);
 			vertex1.y = stod(parametrSet[2]);
@@ -123,8 +111,27 @@ int main()
 			Point vertex3;
 			vertex1.x = stod(parametrSet[5]);
 			vertex1.y = stod(parametrSet[6]);
-			
+
 			shapeSet.push_back(make_unique<Triangle>(vertex1, vertex2, vertex3, stol(parametrSet[7], 0, 16), stol(parametrSet[8], 0, 16)));
+		} },
+	};
+
+	for (;;)
+	{
+		vector <string> parametrSet = readCommand();
+
+		if (cin.eof())
+		{
+			break;
+		}
+
+		else
+		{
+			auto it = actionMap.find(parametrSet[0]);
+			if (it != actionMap.end())
+			{
+				it->second(parametrSet, shapeSet);
+			}
 		}
 	}
 
