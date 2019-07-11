f8fee73fc40bd75decc05a7e7dfd4c95a42461ed
MiNeBudemRabami <sho_mLyaaa@mail.ru>
2019-07-12 01:23:32 +0300

    ...

---
+++ lab_1/1_1_4/auto/auto/auto.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ifstream etalon1(argv[2]);
+	ifstream result1(argv[1]);
+
+	bool isequal = true;
+	char ResChar, EtalChar;
+	for (;;)
+	{
+		ResChar = result1.get();
+
+		EtalChar = etalon1.get();
+
+		if (ResChar == EtalChar)
+		{
+			isequal = true;
+		}
+		else
+		{
+			isequal = false;
+			break;
+		}
+
+		if (result1.eof() && etalon1.eof())
+		{
+			isequal = true;
+			break;
+		}
+
+		if ((!result1.eof() && etalon1.eof()) || (result1.eof() && etalon1.eof()))
+		{
+			isequal = false;
+			break;
+		}
+	}
+	if (isequal)
+	{
+		cout <<  "test is done" << endl;
+	}
+	else
+	{
+		cout << "test falled" << endl;
+	}
+}
--- lab_4/4_1_2/4_1_2/ICanvas.h
+++ lab_4/4_1_2/4_1_2/ICanvas.h
@@ -7,6 +7,9 @@
 class ICanvas
 {
 public:
+
+	virtual ~ICanvas() = 0;
+
 	virtual void DrawLine(Point from, Point to, uint32_t lineColor) = 0;
 	virtual void FillPolygon(std::vector<Point> const& points, uint32_t fillColor) = 0;
 	virtual void DrawCircle(Point center, double radius, uint32_t lineColor) = 0;
